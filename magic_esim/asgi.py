86354e18f7346c86785b16393acf84deb3f131ed
Samuel Akinyemi <akinyemisamuel170@gmail.com>
2024-12-16 17:09:08 +0100

    first commit

---
+++ magic_esim/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for magic_esim project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'magic_esim.settings')
+
+application = get_asgi_application()
