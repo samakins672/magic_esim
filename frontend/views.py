7edb335823fa0e23c5b62aa6557c3b30a0d04991
Samuel Akinyemi <akinyemisamuel170@gmail.com>
2024-12-25 18:58:23 +0100

    implement custom login view and enhance index view with country data

--- frontend/views.py
+++ frontend/views.py
@@ -1,14 +1,37 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView
+import json
+import os
+
+
+class CustomLoginView(LoginView):
+    template_name = 'login.html'
 
 def index(request):
-    return render(request, 'index.html')
+    # Load all countries
+    countries_json_path = os.path.join('static', 'vendor', 'locations', 'countries.json')
+    with open(countries_json_path, 'r') as file:
+        countries = json.load(file)
+
+    # Load popular countries
+    popular_countries_json_path = os.path.join('static', 'vendor', 'locations', 'popular_countries.json')
+    with open(popular_countries_json_path, 'r') as file:
+        popular_countries = json.load(file)
+
+    # Add `is_popular` key to each country
+    popular_country_codes = {country.get('alpha_2') for country in popular_countries if 'alpha_2' in country}
+    for country in countries:
+        country['is_popular'] = country.get('alpha_2') in popular_country_codes if 'alpha_2' in country else False
+
+    # Pass both datasets to the template
+    return render(request, 'index.html', {
+        'countries': countries,
+        'popular_countries': popular_countries
+    })
 
 def signup(request):
     return render(request, 'signup.html')
-
-def login(request):
-    return render(request, 'login.html')
 
 def reset_password(request):
     return render(request, 'reset-password.html')
--- magic_esim/views.py
+++ magic_esim/views.py
@@ -1,29 +1,5 @@
 from django.shortcuts import render
-import json
-import os
+
 
 def custom_404(request, exception):
     return render(request, '404.html', status=404)
-
-
-def index(request):
-    # Load all countries
-    countries_json_path = os.path.join('static', 'vendor', 'locations', 'countries.json')
-    with open(countries_json_path, 'r') as file:
-        countries = json.load(file)
-
-    # Load popular countries
-    popular_countries_json_path = os.path.join('static', 'vendor', 'locations', 'popular_countries.json')
-    with open(popular_countries_json_path, 'r') as file:
-        popular_countries = json.load(file)
-
-    # Add `is_popular` key to each country
-    popular_country_codes = {country.get('alpha_2') for country in popular_countries if 'alpha_2' in country}
-    for country in countries:
-        country['is_popular'] = country.get('alpha_2') in popular_country_codes if 'alpha_2' in country else False
-
-    # Pass both datasets to the template
-    return render(request, 'index.html', {
-        'countries': countries,
-        'popular_countries': popular_countries
-    })
