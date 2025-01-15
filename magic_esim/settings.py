16da4d674bb39a3a5cc2a8891d225c571f9ba5f0
Samuel Akinyemi <akinyemisamuel170@gmail.com>
2025-01-15 16:05:39 +0100

    Made ACCESS_TOKEN_LIFETIME longer

--- magic_esim/settings.py
+++ magic_esim/settings.py
@@ -188,8 +188,8 @@ REST_FRAMEWORK = {
 
 # Simple JWT Configuration
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
     'AUTH_HEADER_TYPES': ('Bearer',),
