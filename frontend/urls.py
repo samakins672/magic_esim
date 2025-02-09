c596954192ec8e49f867da0e92070e3278dc2438
akinyemi samuel <akinyemisamuel170@gmail.com>
2025-02-09 14:02:57 +0100

    Add sitemap.xml template and update URLs to include sitemap route

--- frontend/urls.py
+++ frontend/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.generic import TemplateView
 from . import views
 
 app_name = 'frontend'
@@ -16,4 +17,5 @@ urlpatterns = [
     path('settings/', views.settings, name='settings'),
     path('privacy-policy/', views.privacy_policy, name='privacy_policy'),
     path('terms-and-conditions/', views.terms_and_conditions, name='terms_and_conditions'),
+    path('sitemap.xml', TemplateView.as_view(template_name="sitemap.xml", content_type='application/xml')),
 ]
